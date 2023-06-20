1198afb3176a02764562a67699cd92e6eb83cffd
Tashi <tashishyangba23@gmail.com>
2023-06-19 23:54:44 -0400

    added styles

--- src/components/Gifcard/GifCard.jsx
+++ src/components/Gifcard/GifCard.jsx
@@ -1,12 +1,15 @@
-import React from 'react'
-
-function GifCard({gif}) {
-    //this loads in our gifs
+import React from "react";
+import "./gifcard.css";
+function GifCard({ gif }) {
+  //this loads in our gifs
   return (
     <div>
-      <img src={gif.images.downsized.url} />
+      <div className="gif">
+        <a href={gif.url} target="_blank"><img src={gif.images.downsized.url}/></a>
+        <p><span>Title:</span> {gif.title}</p>
+      </div>
     </div>
-  )
+  );
 }
 
-export default GifCard
+export default GifCard;
