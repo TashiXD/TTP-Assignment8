6b860380405cbb9de00184227506e194697c5389
Tashi <tashishyangba23@gmail.com>
2023-06-19 23:01:25 -0400

    loading gif cards based on trending or search

--- src/App.js
+++ src/App.js
@@ -10,58 +10,76 @@ import Search from "./components/Search/Search";
 function App() {
   const API_KEY = "qFb2hJrxjWy2Ezfe5hkxMyK3nk7nL2DR";
   const [gifs, setGifs] = useState([]);
-  const [trendingGif, setTrendingGif] = useState([]);
   const [search, setSearch] = useState("");
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
 
   //fetches gifs using the trending api
   //and sets the trendingGif and gif to the list of gifs returned
   //using the api
-  useEffect(() => {
-    async function fetchTrendingGif() {
-      try {
-        const response = await axios.get(
-          `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
-        );
-        setIsLoaded(true);
-        setTrendingGif(response.data.data);
-        setGifs(trendingGif);
-      } catch (error) {
-        console.log(error);
-      }
-    }
-    fetchTrendingGif();
-  }, []);
 
-  //function that fetches gif based on what what searched
-  const getSearchGifs = async () => {
+  const fetchTrendingGifs = async () => {
     try {
+      setIsLoading(true);
       const response = await axios.get(
-        `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}`
+        `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
       );
       setGifs(response.data.data);
+      setIsLoading(false);
     } catch (error) {
       console.log(error);
     }
   };
 
+  //function that fetches gif based on what what searched
+  const fetchSearchGifs = async () => {
+    try {
+      setIsLoading(true);
+      let response;
+      if (search !== "") {
+        response = await axios.get(
+          `http://api.giphy.com/v1/gifs/search?q=${search}&api_key=${API_KEY}`
+        );
+      } else {
+        response = await axios.get(
+          `https://api.giphy.com/v1/gifs/trending?api_key=${API_KEY}`
+        );
+      }
+      setGifs(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    fetchTrendingGifs();
+  }, []);
+
+  useEffect(() => {
+    if (search !== "") {
+      fetchSearchGifs();
+    }
+  }, [search]);
+
   return (
     <div className="App">
       <nav>
         <h1>Gif Finder</h1>
-        <Search setSearch={setSearch} getSearchGifs={getSearchGifs} />
+        <Search
+          search={search}
+          setSearch={setSearch}
+          fetchSearchGifs={fetchSearchGifs}
+        />
       </nav>
       <main>
-        {trendingGif ? (
-          gifs.map((gif) => {
-            return (
-              <div key={gif.id}>
-                <GifCard gif={gif} />
-              </div>
-            );
-          })
+        {!isLoading ? (
+          gifs.map((gif, index) => (
+            <div key={index}>
+              <GifCard gif={gif} />
+            </div>
+          ))
         ) : (
-          <img src="https://upload.wikimedia.org/wikipedia/commons/b/b1/Loading_icon.gif?20151024034921%22%20decoding=%22async" />
+          <h1>Loading...</h1>
         )}
       </main>
     </div>
